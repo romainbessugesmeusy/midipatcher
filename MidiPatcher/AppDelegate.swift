5479ae006f459205f9f57ca147f18c76ada91f72
Romain Bessuges-Meusy <romain.bessuges@main-gauche.com>
2020-05-08 22:05:17 +0200

    Initial Commit

---
+++ MidiPatcher/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MidiPatcher
+//
+//  Created by Romain Bessuges-Meusy on 08/05/2020.
+//  Copyright © 2020 Romain Bessuges-Meusy. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
